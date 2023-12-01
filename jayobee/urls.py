5f7361670ea5ee33eee567c850f0f881c154585f
Eric Frey <ericlfrey@gmail.com>
2023-12-01 14:15:19 -0600

    Tested Check User and Register User functions.

--- jayobee/urls.py
+++ jayobee/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from jayobeeapi.views.auth import register_user, check_user
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
+    path('register', register_user),
+    path('checkuser', check_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
--- jayobeeapi/views/auth.py
+++ jayobeeapi/views/auth.py
@@ -1,19 +1,20 @@
-from jayobeeapi.models import Seeker
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from jayobeeapi.models import Seeker
 
 
 @api_view(['POST'])
 def check_user(request):
     '''Checks to see if User has Associated Seeker
 
-    Method arguments:
-      request -- The full HTTP request object
+    Method arguments: request -- The full HTTP request object
     '''
-    uid = request.data['uid']
+    uid = request.META['HTTP_AUTHORIZATION']
+    # uid = request.data['uid']
 
     # Use the built-in authenticate method to verify
     # authenticate returns the user object or None if no user is found
+    # pylint: disable=E1101
     seeker = Seeker.objects.filter(uid=uid).first()
 
     # If authentication was successful, respond with their token
@@ -26,25 +27,25 @@ def check_user(request):
         return Response(data)
     else:
         # Bad login details were provided. So we can't log the user in.
-        data = { 'valid': False }
+        data = {'valid': False}
         return Response(data)
 
 
 @api_view(['POST'])
 def register_user(request):
-    '''Handles the creation of a new Seeker for authentication
+    '''Handles the creation of a new seeker for authentication
 
-    Method arguments:
-      request -- The full HTTP request object
+    Method arguments: request -- The full HTTP request object
     '''
 
-    # Now save the user info in the levelupapi_Seeker table
+    # Uses the built-in Django method to create a new user
+    # pylint: disable=E1101
     seeker = Seeker.objects.create(
-        bio=request.data['bio'],
-        uid=request.data['uid']
+        uid=request.META['HTTP_AUTHORIZATION'],
+        username=request.data['username'],
     )
 
-    # Return the Seeker info to the client
+    # Return the client info to the client
     data = {
         'id': seeker.id,
         'uid': seeker.uid,
